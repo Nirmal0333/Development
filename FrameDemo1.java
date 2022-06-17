9b76d215babad837262d6c10b1ce27b400ec6942
Nirmal0333 <106113587+Nirmal0333@users.noreply.github.com>
2022-06-17 12:16:35 +0530

    Add files via upload

---
+++ FrameDemo1.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.awt.*;
+
+class FrameDemo1
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Print data on console");
+		
+		Frame fobj = new Frame("Marvellous");
+		fobj.setSize(400,400);
+		fobj.SetVisible(true);
+	}
+}
